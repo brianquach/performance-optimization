391de465366ede38fb2b09f01ff6bcf1f92ac758
Brian Quach <13rianquach@gmail.com>
2016-11-29 17:44:26 -0800

    Refact: minify js and css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var imageResize = require('gulp-image-resize');
 var rename = require('gulp-rename');
+var cleanCSS = require('gulp-clean-css');
+var minifyJS = require('gulp-minify');
 var tasks = [];
 
 var PIZZA_IMG_MIN_TASK = 'pizza_images_min';
@@ -50,5 +52,54 @@ sizes.forEach(function (size) {
   });
   tasks.push(taskName);
 });
+
+var MINIFY_JS = 'minify_js';
+gulp.task(MINIFY_JS, function () {
+  gulp.src('src/js/main/*.js')
+    .pipe(minifyJS({
+        ext:{
+            min:'-min.js'
+        },
+        ignoreFiles: ['-min.js']
+    }))
+    .pipe(gulp.dest('dist/js'))
+
+  gulp.src('src/js/pizza/*.js')
+    .pipe(minifyJS({
+        ext:{
+            min:'-min.js'
+        },
+        ignoreFiles: ['-min.js']
+    }))
+    .pipe(gulp.dest('dist/views/js'))
+});
+tasks.push(MINIFY_JS);
+
+var MINIFY_CSS = 'minify_css';
+gulp.task(MINIFY_CSS, function () {
+  gulp.src('src/css/main/*.css')
+    .pipe(
+      cleanCSS({
+        compatibility: 'ie8'}
+      )
+    )
+    .pipe(rename(function (path) {
+      path.basename += '-min';
+    }))
+    .pipe(gulp.dest('dist/css'));
+
+  gulp.src('src/css/pizza/*.css')
+    .pipe(
+      cleanCSS({
+        compatibility: 'ie8'}
+      )
+    )
+    .pipe(rename(function (path) {
+      path.basename += '-min';
+    }))
+    .pipe(gulp.dest('dist/views/css'));
+});
+tasks.push(MINIFY_CSS);
+
 
 gulp.task('optimize', tasks);
